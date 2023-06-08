baf92c9ccaf4bf98f7a054871b85b79de0eb631e
Dhruvil27 <103164455+Dhruvil27@users.noreply.github.com>
2023-06-08 13:59:15 +0530

    Add files via upload

---
+++ Pr28.java
@@ -0,0 +1,42 @@
+import 	java.util.ArrayList;
+import	java.util.LinkedList;
+import	java.util.List;
+
+
+public class Pr28
+
+{
+
+public static void main(String[] args)
+{
+
+List<String> weekdays=new ArrayList<>();
+
+weekdays.add("Monday");
+weekdays.add("Tuesday");
+weekdays.add("Wednesday");
+weekdays.add("Thursday");
+weekdays.add("Friday");
+weekdays.add("Saturday");
+weekdays.add("Sunday");
+
+
+System.out.println(weekdays);
+
+
+
+List<String> Months = new LinkedList<>();
+
+Months.add("Monday");
+Months.add("February");
+Months.add("Wednesday");
+Months.add("March");
+Months.add("April");
+Months.add("May");
+Months.add("June");
+
+System.out.println(Months);
+
+
+}
+}
