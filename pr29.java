fcd1fb4e2c4e3696e382c7a5b4764c5940cf8a86
Dhruvil27 <103164455+Dhruvil27@users.noreply.github.com>
2023-06-08 22:42:41 -0600

    Add files via upload

---
+++ pr29.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Pr29
+{
+public static void main(String[]args)
+{	
+	Hashset<String>colorset =new Hashset();
+	
+		colorset.add("Yellow");
+		colorset.add("Red");
+		colorset.add("skyblue");
+		colorset.add("White");
+		colorset.add("blue");
+		colorset.add("Green");
+
+			for(String colors:colorset)
+			{
+				System.out.println(colors);
+			
+			}
+
+}
+}
